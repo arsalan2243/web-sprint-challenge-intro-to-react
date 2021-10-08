9c82464d68373999b35ab9bfea87d2ea216e63a0
arsalan rahimi <years1373@yahoo.com>
2021-10-08 13:01:53 -0700

    charcters named

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import Characters from './components/Character'
 
 const App = () => {
+  const [data, setData]=useState([])
+  useEffect(() => {
+    axios.get ('https://swapi.dev/api/people')
+    .then(response => {
+      setData (response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, []
+  )
   // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  // the state properties here., 
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,6 +25,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.map (character => (<Characters character={character}/>))}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+import react  from 'react'
+import styled from 'styled-components'
+
+const Div = styled.div`
+background-color: rgb(0, 0, 0 ,0.5);;
+color: white;
+max-width: 100%;
+ border: 1px solid rgb(210, 210, 210);
+  box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+  border-radius: 8px;
+  margin: 16px;
+  padding: 16px 0px 12px 0px;
+  display: flex;
+  justify-content: space-evenly;
+  cursor: pointer;
+`;
+
+const Characters = (props) => {
+    const character = props.character
+    return (
+        <Div>
+            <h2>{character.name}</h2>
+            <h2>{character.birth_year}</h2>
+        </Div>
+    )
+}
+
+export default Characters
